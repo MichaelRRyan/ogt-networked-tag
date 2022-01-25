4557692f24497aa414ecb76a74323d7900e1c94d
SzymonPiskorz <c00242808@itcarlow.ie>
2022-01-25 22:30:56 +0000

    Added most of the player functions and code

---
+++ ogt-networked-tag/include/Player.h
@@ -0,0 +1,66 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <SFML\Graphics.hpp>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include "Globals.h"
+#include "Cell.h"
+
+class Player : public sf::Drawable
+{
+	const int MAX_HURT_TIME{ 60 };
+	const int MOVEMENT_TIME{ 15 };
+	static const int MAX_CHARACTERS{ 10 };
+
+	sf::Texture m_spriteSheet;
+	sf::Sprite m_body;
+	sf::Vector2i m_pos;
+	sf::Vector2i m_previousPos;
+	int m_score;
+	int m_lives;
+	int m_hurtTimer;
+	int m_moveTimer;
+
+	sf::Vector2i m_characterPositions[MAX_CHARACTERS];
+	int m_characterDirection;
+	int m_characterNumber;
+	sf::Vector2i m_character;
+
+	sf::Font m_font;
+	sf::Text m_playerName;
+
+public:
+	Player(sf::Font);
+	void loadFiles();
+	void respawn();
+
+
+
+	virtual void draw(sf::RenderTarget& t_target, sf::RenderStates t_states) const;
+
+	void setPos(sf::Vector2i t_pos);
+	inline void setLives(int t_lives) { m_lives = t_lives; }
+	inline void setPosition(sf::Vector2f t_position) { m_body.setPosition(t_position); }
+	inline void setScale(sf::Vector2f t_scale) { m_body.setScale(t_scale); }
+	inline void refreshPosition() { m_body.setPosition(static_cast<float>(m_pos.x * TILE_SIZE), static_cast<float>(m_pos.y * TILE_SIZE)); }
+	void setCharacter(int t_characterNum);
+
+	inline sf::Vector2i getPos() { return m_pos; } 
+	inline sf::Sprite getBody() { return m_body; }
+	inline int getScore() { return m_score; }
+	inline int getLives() { return m_lives; }
+	inline int getCharNum() { return m_characterNumber; }
+
+	void move(Direction t_direction, Cell t_maze[][MAX_COLS]);
+	void setTextureDirection(Direction t_direction);
+	void update(Cell t_maze[][MAX_COLS]);
+	void movementInput(Cell t_maze[][MAX_COLS]);
+	void animations();
+
+	inline void setCharName(std::string t_str) { m_playerName.setString(t_str); }
+};
+
+#endif // !PLAYER_H
---
+++ ogt-networked-tag/src/Player.cpp
@@ -0,0 +1,168 @@
+#include "Player.h"
+
+Player::Player(sf::Font t_font) :
+	m_characterPositions{
+{ CHAR_SPACING * 5, CHAR_HEIGHT * 24 },
+{ CHAR_SPACING * 5, CHAR_HEIGHT * 27},
+{ CHAR_SPACING * 6, CHAR_HEIGHT * 13},
+{ CHAR_SPACING * 11, CHAR_HEIGHT * 13},
+{ CHAR_SPACING * 11, CHAR_HEIGHT * 17},
+{ CHAR_SPACING * 1, 0},
+{ CHAR_SPACING * 11, 0},
+{ CHAR_SPACING * 8, CHAR_HEIGHT * 9},
+{ CHAR_SPACING * 1, CHAR_HEIGHT * 16},
+{ CHAR_SPACING * 1, CHAR_HEIGHT * 30} }
+{
+	m_font = t_font;
+	m_playerName.setFont(m_font);
+
+	loadFiles();
+	respawn();
+
+	m_score = 0;
+}
+
+void Player::loadFiles()
+{
+	if (!m_spriteSheet.loadFromFile("ASSETS\\IMAGES\\characters.png"))
+	{
+		// Error loading image
+	}
+
+	m_body.setTexture(m_spriteSheet);
+	m_body.setTextureRect(sf::IntRect{ CHAR_SPACING * 2,0,CHAR_WIDTH,CHAR_HEIGHT });
+	m_body.setOrigin(0.0f, static_cast<float>(CHAR_WIDTH));
+}
+
+void Player::respawn()
+{
+	m_score = 0;
+	m_hurtTimer = 0;
+	m_moveTimer = 0;
+	m_characterDirection = 0;
+	m_lives = 3;
+	m_body.setTextureRect(sf::IntRect{ m_character.x + CHAR_SPACING,m_character.y,CHAR_WIDTH,CHAR_HEIGHT });
+	m_body.setColor(sf::Color::White);
+	m_body.setOrigin(0.0f, m_body.getOrigin().y);
+	m_body.setScale(1.0f, 1.0f);
+	setPos(sf::Vector2i{ 12,2 });
+
+}
+
+void Player::draw(sf::RenderTarget& t_target, sf::RenderStates t_states) const
+{
+	t_target.draw(m_body, t_states);
+	t_target.draw(m_playerName, t_states);
+}
+
+void Player::setPos(sf::Vector2i t_pos)
+{
+	m_pos = t_pos;
+	m_previousPos = t_pos;
+	m_body.setPosition(static_cast<sf::Vector2f>(t_pos * 32));
+}
+
+void Player::setCharacter(int t_characterNum)
+{
+	m_characterNumber = t_characterNum % MAX_CHARACTERS;
+	if (m_characterNumber < 0)
+		m_characterNumber = MAX_CHARACTERS + m_characterNumber;
+	m_character = m_characterPositions[m_characterNumber];
+	m_body.setTextureRect(sf::IntRect{ m_character.x + CHAR_SPACING,m_character.y,CHAR_WIDTH,CHAR_HEIGHT });
+}
+
+void Player::move(Direction t_direction, Cell t_maze[][MAX_COLS])
+{
+	sf::Vector2i dirVector = Global::getDirectionVector(t_direction);
+	sf::Vector2i desiredPosition = m_pos + dirVector;
+
+	if (t_maze[desiredPosition.y][desiredPosition.x].getTileType() != Tile::Rock) // Move if not blocked by a rock
+	{
+		if (t_maze[desiredPosition.y][desiredPosition.x].getTileType() != Tile::Moveable) // Move if not blocked by a moveable block
+		{
+			m_previousPos = m_pos;
+			m_pos = desiredPosition;
+			m_moveTimer = MOVEMENT_TIME;
+		}
+	}
+
+	setTextureDirection(t_direction);
+
+	if (t_maze[m_pos.y][m_pos.x].getTileType() == Tile::Coin)
+	{
+		t_maze[m_pos.y][m_pos.x].setTileType(Tile::None);
+		m_score++;
+	}
+}
+
+void Player::setTextureDirection(Direction t_direction)
+{
+	if (t_direction == Direction::North)
+		m_characterDirection = 2;
+	else if (t_direction == Direction::South)
+		m_characterDirection = 0;
+	else if (t_direction == Direction::West)
+	{
+		m_body.setScale(-1.0f, 1.0f);
+		m_body.setOrigin(static_cast<float>(CHAR_WIDTH), m_body.getOrigin().y);
+		m_characterDirection = 1;
+	}
+	else if (t_direction == Direction::East)
+	{
+		m_body.setScale(1.0f, 1.0f);
+		m_body.setOrigin(0.0f, m_body.getOrigin().y);
+		m_characterDirection = 1;
+	}
+}
+
+void Player::update(Cell t_maze[][MAX_COLS])
+{
+	if (m_moveTimer == 0)
+	{
+		movementInput(t_maze);
+		m_body.setTextureRect(sf::IntRect{ m_character.x + CHAR_SPACING,m_character.y + m_characterDirection * CHAR_HEIGHT,CHAR_WIDTH,CHAR_HEIGHT });
+	}
+	else
+		animations();
+
+	if (m_hurtTimer > 0)
+	{
+		m_hurtTimer--;
+		if (m_hurtTimer == 0)
+			m_body.setColor(sf::Color::White);
+	}
+}
+
+void Player::movementInput(Cell t_maze[][MAX_COLS])
+{
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+	{
+		move(Direction::North, t_maze);
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+	{
+		move(Direction::South, t_maze);
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+	{
+		move(Direction::East, t_maze);
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+	{
+		move(Direction::West, t_maze);
+	}
+}
+
+void Player::animations()
+{
+	m_moveTimer--;
+
+	float newX = (m_pos.x * 32) * (1.0f - (1.0f * m_moveTimer / MOVEMENT_TIME)) + (m_previousPos.x * 32) * (1.0f * m_moveTimer / MOVEMENT_TIME);
+	float newY = (m_pos.y * 32) * (1.0f - (1.0f * m_moveTimer / MOVEMENT_TIME)) + (m_previousPos.y * 32) * (1.0f * m_moveTimer / MOVEMENT_TIME);
+
+	m_body.setPosition(static_cast<float>(newX), static_cast<float>(newY));
+
+	int frameNum = static_cast<int>((1.0 * m_moveTimer / MOVEMENT_TIME) * 3);
+
+	m_body.setTextureRect(sf::IntRect{ m_character.x + (CHAR_SPACING * frameNum), m_character.y + (m_characterDirection * CHAR_HEIGHT),CHAR_WIDTH, CHAR_HEIGHT });
+}
